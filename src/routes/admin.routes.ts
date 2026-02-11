7e007dc56c0bd750b1d12d55adf9bc9060052682
ananthu prasad <ananthu@voxbaysolutions.com>
2026-02-11 09:02:23 +0530

    changes

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addStudent, assignTask } from "../controllers/admin.controller";
+import { addStudent, assignTask } from "../controllers/admin.controller.ts";
 import { auth } from "../middleware/auth.middleware";
 import { isAdmin } from "../middleware/role.middleware";
 
